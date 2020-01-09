b002be85051f4409454a65a839240be04ad3601b
Paul van Buuren <paul@wbvb.nl>
2020-01-09 16:48:57 +0100

    3.29.2 - Footer-widget voor menu voorzien van pijltjes.

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
 // @package gebruiker-centraal
 // @author  Paul van Buuren
 // @license GPL-2.0+
-// @version 3.29.1
-// @desc.   Public Service nominatie-widget op homepage.
+// @version 3.29.2
+// @desc.   Footer-widget voor menu voorzien van pijltjes.
 // @link    https://github.com/ICTU/gebruiker-centraal-wordpress-theme
 
 
@@ -23,8 +23,8 @@ require_once( get_template_directory() . '/lib/init.php' );
  */
 define( 'CHILD_THEME_NAME', 'Gebruiker Centraal' );
 define( 'CHILD_THEME_URL', 'https://wbvb.nl/themes/gebruikercentraal' );
-define( 'CHILD_THEME_VERSION', '3.29.1' );
-define( 'CHILD_THEME_DESCRIPTION', "3.29.1 - Public Service nominatie-widget op homepage." );
+define( 'CHILD_THEME_VERSION', '3.29.2' );
+define( 'CHILD_THEME_DESCRIPTION', "3.29.2 - Footer-widget voor menu voorzien van pijltjes." );
 
 define( 'GC_TWITTERACCOUNT', 'gebrcentraal' );
 define( 'GC_TWITTER_URL', 'https://twitter.com/' );
