35ea4dfca12d2fe7971283edcf49e5856aa14a93
Paul van Buuren <paul@wbvb.nl>
2020-02-17 17:52:59 +0100

    nieuwe rol ook hier toegevoegd

--- page_alle-auteurs.php
+++ page_alle-auteurs.php
@@ -7,8 +7,8 @@
 // @package gebruiker-centraal
 // @author  Paul van Buuren
 // @license GPL-2.0+
-// @version 3.7.7
-// @desc.   Added author overview page
+// @version 4.3.1
+// @desc.   Fixes voor actieteamwidget bug (sanitize_title) en authorbox (get user ID from get_queried_object).
 // @link    https://github.com/ICTU/gebruiker-centraal-wordpress-theme
 
 
@@ -16,32 +16,41 @@
 
 add_action( 'genesis_entry_content', 'gc_wbvb_show_alle_auteurs', 11 );
 
+//========================================================================================================
 
 function filter_two_roles($user) {
-    $roles = array('contributor',',author','editor','administrator');
+
+	// nieuwe rol ook hier toegevoegd	
+	// * @since	  4.3.1	
+	$roles = array('contributor',',author','editor','administrator','rdamadmin');
     return in_array($user->roles[0], $roles);
 }
 
+//========================================================================================================
 
 function gc_wbvb_show_alle_auteurs() {
+	
+	$users = get_users('fields=all_with_meta');
 
-  $users = get_users('fields=all_with_meta');
+	echo '<div class="actieteam">';
+	
+	// Iterate through users, filtering out the ones which don't have the roles we want 
+	foreach(array_filter($users, 'filter_two_roles') as $user) {
+//	foreach( $users as $user) {
 
-
-  echo '<div class="actieteam">';
-
-  // Iterate through users, filtering out the ones which don't have the roles we want 
-  foreach(array_filter($users, 'filter_two_roles') as $user) {
-    $user_post_count        = count_user_posts( $user->ID ); 
-    if ( $user_post_count > 0 ) {
-      echo  gc_wbvb_authorbox_compose_box( $user->ID );
-    }
-  }
-
-  echo '</div>';
-
+//echo 'Rol: ' . $user->roles[0] . '<br>';
+		
+		$user_post_count        = count_user_posts( $user->ID ); 
+		if ( $user_post_count > 0 ) {
+			echo  gc_wbvb_authorbox_compose_box( $user->ID );
+		}
+	}
+	
+	echo '</div>';
+	
 }
 
+//========================================================================================================
 
 genesis();
 
