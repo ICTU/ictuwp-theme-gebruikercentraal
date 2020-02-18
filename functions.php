cc0cc168ba748af5d9513bdb32fd9fa150efafb7
Paul van Buuren <paul@wbvb.nl>
2020-02-18 11:39:38 +0100

    Versienummer opgehoogd, om te voorkomen dat bezoekers een brakke blogarchiefpagina zien

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
 // @package gebruiker-centraal
 // @author  Paul van Buuren
 // @license GPL-2.0+
-// @version 4.3.1
-// @desc.   Fixes voor actieteamwidget bug (sanitize_title) en authorbox (get user ID from get_queried_object).
+// @version 4.3.2
+// @desc.   Spoedreparatie blogarchiefpagina.
 // @link    https://github.com/ICTU/gebruiker-centraal-wordpress-theme
 
 
@@ -23,8 +23,8 @@ require_once( get_template_directory() . '/lib/init.php' );
  */
 define( 'CHILD_THEME_NAME', 'Gebruiker Centraal' );
 define( 'CHILD_THEME_URL', 'https://wbvb.nl/themes/gebruikercentraal' );
-define( 'CHILD_THEME_VERSION', '4.3.1' );
-define( 'CHILD_THEME_DESCRIPTION', "4.3.1 - Fixes voor actieteamwidget bug (sanitize_title) en authorbox (get user ID from get_queried_object)." );
+define( 'CHILD_THEME_VERSION', '4.3.2' );
+define( 'CHILD_THEME_DESCRIPTION', "4.3.2 - Spoedreparatie blogarchiefpagina." );
 
 define( 'GC_TWITTERACCOUNT', 'gebrcentraal' );
 define( 'GC_TWITTER_URL', 'https://twitter.com/' );
