13f891f9d627ffa5c10f795271a6b39ab4c84e8d
Paul van Buuren <paul@wbvb.nl>
2020-04-09 09:41:07 +0200

    reformatting

--- includes/textblock-home.php
+++ includes/textblock-home.php
@@ -57,7 +57,6 @@ if ( ! function_exists( 'ictu_gctheme_frontend_general_get_textblocks' ) ) :
 		// Parse incoming $args into an array and merge it with $defaults
 		$args = wp_parse_args( $args, $defaults );
 
-
 		if ( function_exists( 'get_field' ) ) {
 
 			$textblocks = get_field( 'textblocks', $post->ID );
@@ -65,9 +64,9 @@ if ( ! function_exists( 'ictu_gctheme_frontend_general_get_textblocks' ) ) :
 			if ( $textblocks ):
 
 				// count the items
-				$countcount    = count( $textblocks );
+				$countcount = count( $textblocks );
 
-				$return = '<section class="section section--text-blocks l-item-count-'. $countcount.'">';
+				$return = '<section class="section section--text-blocks l-item-count-' . $countcount . '">';
 				$return .= '<div class="l-section-content">';
 
 				while ( have_rows( 'textblocks' ) ): the_row();
@@ -78,8 +77,8 @@ if ( ! function_exists( 'ictu_gctheme_frontend_general_get_textblocks' ) ) :
 					$title_id          = sanitize_title( $textblock_title . '-title' );
 
 					// Add class for color if value not is none
-					if(!(get_sub_field( 'textblock_class') === 'none')) {
-						$textblock_class .= ' '.get_sub_field( 'textblock_class');
+					if ( ! ( get_sub_field( 'textblock_class' ) === 'none' ) ) {
+						$textblock_class .= ' ' . get_sub_field( 'textblock_class' );
 					}
 
 					$return .= '<section aria-labelledby="' . $title_id . '" class="' . $textblock_class . '">';
