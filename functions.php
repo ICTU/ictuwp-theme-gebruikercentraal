b31ae6f03d40a492ab005d84691dca3b02689381
Paul van Buuren <paul@wbvb.nl>
2020-12-02 17:10:20 +0100

    tijdelijk versienummer om te testen op accept

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 // @package gebruiker-centraal
 // @author  Paul van Buuren
 // @license GPL-2.0+
-// @version 4.3.17
+// @version 4.3.17.a.2
 // @desc.   Mogelijkheid om uitgelichte afbeelding te verbergen toegevoegd aan GC_berichten_widget.
 // @link    https://github.com/ICTU/gebruiker-centraal-wordpress-theme
 
@@ -23,8 +23,8 @@ require_once( get_template_directory() . '/lib/init.php' );
  */
 define( 'CHILD_THEME_NAME', 'Gebruiker Centraal' );
 define( 'CHILD_THEME_URL', 'https://wbvb.nl/themes/gebruikercentraal' );
-define( 'CHILD_THEME_VERSION', '4.3.17' );
-define( 'CHILD_THEME_DESCRIPTION', "4.3.17 - Mogelijkheid om uitgelichte afbeelding te verbergen toegevoegd aan GC_berichten_widget." );
+define( 'CHILD_THEME_VERSION', '4.3.17.a.2' );
+define( 'CHILD_THEME_DESCRIPTION', "4.3.17.a.2 - Mogelijkheid om uitgelichte afbeelding te verbergen toegevoegd aan GC_berichten_widget." );
 
 define( 'GC_TWITTERACCOUNT', 'gebrcentraal' );
 define( 'GC_TWITTER_URL', 'https://twitter.com/' );
