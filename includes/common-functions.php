ed7daac2b7ac2240ae92087607e68945aef0c73e
Paul van Buuren <paul@wbvb.nl>
2021-06-07 19:46:36 +0200

    placeholder en label voor zoekformulier gelijkgetrokken

--- includes/common-functions.php
+++ includes/common-functions.php
@@ -56,23 +56,36 @@ function gc_wbvb_add_id_to_search_form( $form ) {
 
 //========================================================================================================
 
-//* Customize search form input box text
-add_filter( 'genesis_search_text', 'gc_wbvb_searchform_text' );
+//* Customize search form label
+add_filter( 'genesis_search_form_label', 'gc_wbvb_searchform_label_text' );
 
-function gc_wbvb_searchform_text( $text ) {
+function gc_wbvb_searchform_label_text( $text ) {
 
-	return esc_attr( _x( "Search...", 'search', 'gebruikercentraal' ) );
+	return esc_attr( _x( "Search this site", 'search label and placeholder text', 'gebruikercentraal' ) );
 
 }
 
 //========================================================================================================
 
-//* Customize search form label
-add_filter( 'genesis_search_form_label', 'gc_wbvb_searchform_label' );
+//* Customize search form input button text
+add_filter( 'genesis_search_button_text', 'gc_wbvb_searchform_submitbutton_text' );
 
-function gc_wbvb_searchform_label( $text ) {
+function gc_wbvb_searchform_submitbutton_text( $text ) {
 
-	return esc_attr( _x( "Search within this site", 'search', 'gebruikercentraal' ) );
+	return esc_attr( _x( "Search", 'search button text', 'gebruikercentraal' ) );
+
+}
+
+//========================================================================================================
+
+//* Customize search form input box text
+add_filter( 'genesis_search_text', 'gc_wbvb_searchform_placeholder_text' );
+
+function gc_wbvb_searchform_placeholder_text( $text ) {
+
+	// this is the placeholder text. This should be equal to the
+	// label text (which is visually hidden)
+	return esc_attr( _x( "Search this site", 'search label and placeholder text', 'gebruikercentraal' ) );
 
 }
 
